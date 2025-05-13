b00f2832ac7dd9a6e25c03500e408b76ad5ef7f2
matute <idkmatt0@gmail.com>
2025-05-13 18:43:30 -0300

    Categories routes andando
    cambio de sintaxis en categories

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const exampleRoutes = require("./exampleRoutes");
 const productRoutes = require("./productsRoutes");
 const orderRoutes = require("./orderRoutes");
 const authRoutes = require("./authRoutes");
+const categoriesRoutes = require("./categoriesRoutes");
 
 module.exports = (app) => {
   app.use("/users", userRoutes);
@@ -10,4 +11,5 @@ module.exports = (app) => {
   app.use("/products", productRoutes);
   app.use("/orders", orderRoutes);
   app.use("/tokens", authRoutes);
+  app.use("/categories", categoriesRoutes);
 };
