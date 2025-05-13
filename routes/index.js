bd9c345d925166baba7f5af5c6046f16c107f7d0
matute <idkmatt0@gmail.com>
2025-05-13 18:11:59 -0300

    Revert "update de index.js"
    This reverts commit d9c9547bc102c13bcb13e81e944c98eefa682378.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const exampleRoutes = require("./exampleRoutes");
 const productRoutes = require("./productsRoutes");
 const orderRoutes = require("./orderRoutes");
 const authRoutes = require("./authRoutes");
-const adminUserRoutes = require("./adminUserRoutes");
 
 module.exports = (app) => {
   app.use("/users", userRoutes);
@@ -11,5 +10,4 @@ module.exports = (app) => {
   app.use("/products", productRoutes);
   app.use("/orders", orderRoutes);
   app.use("/tokens", authRoutes);
-  app.use("/ausers", adminUserRoutes);
 };
