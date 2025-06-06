29e61c4ffd684fbb0eb6ff4ffb95bb4a8c9f9213
matute <idkmatt0@gmail.com>
2025-06-05 22:11:44 -0300

    Agregado para eliminar duplicados

--- seeders/productsSeeder.js
+++ seeders/productsSeeder.js
@@ -1,14 +1,17 @@
 const { Products } = require("../models");
 
 module.exports = async () => {
+  // Elimina productos existentes para evitar duplicados
+  await Products.destroy({ where: {} });
+
   const products = [
     {
-      name: 'EQUILIBRIO Cachorro 15kg"',
+      name: "EQUILIBRIO Cachorro 15kg",
       description:
-        "alimento completo diseñado para perros en fase de crecimiento, desde los 2 meses hasta los 12, 18 o 24 meses, dependiendo de la raza",
+        "Alimento completo diseñado para perros en fase de crecimiento, desde los 2 meses hasta los 12, 18 o 24 meses, dependiendo de la raza.",
       image:
         "https://purina.com.uy/sites/default/files/styles/webp/public/2022-09/ADULTOS%20PERROS%20MINIS%20Y%20PEQUEN%CC%83OS-dog-chow-frente.jpeg.webp?itok=UwtgKL2x",
-      price: 3.783,
+      price: 3783,
       stock: 15,
       category: "Secos",
       featured: true,
@@ -16,10 +19,10 @@ module.exports = async () => {
     {
       name: "Collar perro 53cm",
       description:
-        "El collar para perro es un accesorio diseñado para brindar comodidad y seguridad a tu mascota",
+        "El collar para perro es un accesorio diseñado para brindar comodidad y seguridad a tu mascota.",
       image:
-        "https://purina.com.uy/sites/default/files/styles/webp/public/2022-09/ADULTOS%20PERROS%20MINIS%20Y%20PEQUEN%CC%83OS-dog-chow-frente.jpeg.webp?itok=UwtgKL2x",
-      price: 185.0,
+        "https://www.doggysmarket.com.co/wp-content/uploads/2025/02/Kong-Collar-Perro-Rojo.png",
+      price: 185,
       stock: 30,
       category: "Collares",
       featured: false,
@@ -28,14 +31,14 @@ module.exports = async () => {
       name: "Comedero mascotas acero inoxidable",
       description: "Fabricado con acero inoxidable diseñado para durar.",
       image:
-        "https://purina.com.uy/sites/default/files/styles/webp/public/2022-09/ADULTOS%20PERROS%20MINIS%20Y%20PEQUEN%CC%83OS-dog-chow-frente.jpeg.webp?itok=UwtgKL2x",
-      price: 350.0,
+        "https://cdn11.bigcommerce.com/s-i8rmeakles/images/stencil/1280x1280/products/466/832/7404_Bella_Bowl_Classic_Espresso_D__86899.1654113396.jpg?c=1",
+      price: 350,
       stock: 10,
       category: "Comedores",
       featured: true,
     },
   ];
-  console.log(Products);
+
   await Products.bulkCreate(products);
-  console.log("[Database] Se corrió el seeder de Products.");
+  console.log("[Database] Se corrió el seeder de Products con 3 productos únicos.");
 };
