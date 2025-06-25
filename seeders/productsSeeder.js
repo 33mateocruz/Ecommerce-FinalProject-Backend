86a9dce6cbc1c70d1283a2fe4199c4efb7ede946
JoaquinRosso <joaquinrosso17@gmail.com>
2025-06-24 21:02:03 -0300

    Update categorieseeder.js
    se cambio subcategoryId por category y todos los featured:false por true

--- seeders/productsSeeder.js
+++ seeders/productsSeeder.js
@@ -11,7 +11,7 @@ module.exports = async () => {
       image: "https://mascotasmimadas.com.uy/wp-content/uploads/2024/06/IM-ROPA-4104-portada.png",
       price: 950,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -20,7 +20,7 @@ module.exports = async () => {
       image: "https://www.tusmascotas.cl/wp-content/uploads/2022/04/chaleco-rojo-.png",
       price: 1100,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -30,7 +30,7 @@ module.exports = async () => {
         "https://www.colettetgaston.com/cdn/shop/files/high_quality_dog_shirt.png?v=1749159105&width=533",
       price: 750,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -39,7 +39,7 @@ module.exports = async () => {
       image: "https://greenpets.com.co/wp-content/uploads/2021/10/abeja-green-pets.png",
       price: 1200,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -48,7 +48,7 @@ module.exports = async () => {
       image: "https://per-ros.com/productos/botitas%20(1)-27.png",
       price: 890,
       stock: 25,
-      subcategoryId: 25,
+      category: 25,
       featured: true,
     },
     {
@@ -57,7 +57,7 @@ module.exports = async () => {
       image: "https://img.chilango.com/2019/10/Captura-de-Pantalla-2019-10-29-a-las-18.34.26.png",
       price: 1300,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -67,7 +67,7 @@ module.exports = async () => {
         "https://png.pngtree.com/png-clipart/20250109/original/pngtree-cowboy-bandana-isolated-white-backdrop-png-image_18970791.png",
       price: 390,
       stock: 25,
-      subcategoryId: 27,
+      category: 27,
       featured: true,
     },
     {
@@ -76,7 +76,7 @@ module.exports = async () => {
       image: "https://nuevaerauruguay.com/wp-content/uploads/2025/04/Dog-Jacket-16.png",
       price: 980,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -86,7 +86,7 @@ module.exports = async () => {
         "https://acdn-us.mitiendanube.com/stores/035/032/products/remera-argentina1-04b04f33c2f1fb6e6816691505601044-640-0.png",
       price: 1400,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -95,7 +95,7 @@ module.exports = async () => {
       image: "https://http2.mlstatic.com/D_NQ_NP_648127-MLU84103751139_042025-O.webp",
       price: 890,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -104,7 +104,7 @@ module.exports = async () => {
       image: "https://http2.mlstatic.com/D_NQ_NP_692935-MLU73066826549_112023-O.webp",
       price: 1250,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -113,7 +113,7 @@ module.exports = async () => {
       image: "https://m.media-amazon.com/images/I/61PZblIY9xL._AC_UF894,1000_QL80_.jpg",
       price: 520,
       stock: 25,
-      subcategoryId: 27,
+      category: 27,
       featured: true,
     },
     {
@@ -122,7 +122,7 @@ module.exports = async () => {
       image: "https://m.media-amazon.com/images/I/71HSgl9eZlL.jpg",
       price: 970,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -131,7 +131,7 @@ module.exports = async () => {
       image: "https://http2.mlstatic.com/D_NQ_NP_941420-MLU51024289085_082022-O.webp",
       price: 1140,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -140,7 +140,7 @@ module.exports = async () => {
       image: "https://m.media-amazon.com/images/I/61XhSQaLV2L.jpg",
       price: 1320,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -149,7 +149,7 @@ module.exports = async () => {
       image: "https://i.pinimg.com/736x/bd/d8/cd/bdd8cd9a868f5b6aba8f319d9bacc92d.jpg",
       price: 990,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -159,7 +159,7 @@ module.exports = async () => {
         "https://frenchiemania.com/wp-content/uploads/2022/04/como-proteger-perro-lluvia-gorros-impermeables-para-perros-2.jpg",
       price: 430,
       stock: 25,
-      subcategoryId: 27,
+      category: 27,
       featured: true,
     },
     {
@@ -168,7 +168,7 @@ module.exports = async () => {
       image: "https://m.media-amazon.com/images/I/71nOKambE3L._AC_UF894,1000_QL80_.jpg",
       price: 1500,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -177,7 +177,7 @@ module.exports = async () => {
       image: "https://m.media-amazon.com/images/I/71SrAD1aCYL._AC_UF1000,1000_QL80_.jpg",
       price: 1380,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -187,7 +187,7 @@ module.exports = async () => {
         "https://cdnx.jumpseller.com/alo-mascotas/image/25122293/e2176496840fb9f9071cc08f1c119315.jpg?1658115894",
       price: 1650,
       stock: 25,
-      subcategoryId: 26,
+      category: 26,
       featured: true,
     },
     {
@@ -197,7 +197,7 @@ module.exports = async () => {
         "https://png.pngtree.com/png-vector/20230915/ourmid/pngtree-red-inflatable-rubber-ball-inflatable-png-image_9244542.png",
       price: 430,
       stock: 25,
-      subcategoryId: 20,
+      category: 20,
       featured: true,
     },
     {
@@ -207,7 +207,7 @@ module.exports = async () => {
         "https://w7.pngwing.com/pngs/197/217/png-transparent-dog-toys-rope-pet-rope-knot-animals-ring-pet-thumbnail.png",
       price: 390,
       stock: 25,
-      subcategoryId: 17,
+      category: 17,
       featured: true,
     },
     {
@@ -217,7 +217,7 @@ module.exports = async () => {
         "https://pet-health-content-media.chewy.com/wp-content/uploads/2025/04/16213146/202504bec-Orbbe-tuff-dog-toy.png",
       price: 460,
       stock: 25,
-      subcategoryId: 19,
+      category: 19,
       featured: true,
     },
     {
@@ -227,7 +227,7 @@ module.exports = async () => {
         "https://png.pngtree.com/png-vector/20250321/ourmid/pngtree-squeaky-toys-isolated-on-a-transparent-background-png-image_15807162.png",
       price: 510,
       stock: 25,
-      subcategoryId: 21,
+      category: 21,
       featured: true,
     },
     {
@@ -236,7 +236,7 @@ module.exports = async () => {
       image: "https://pngimg.com/d/frisbee_PNG6.png",
       price: 580,
       stock: 25,
-      subcategoryId: 22,
+      category: 22,
       featured: true,
     },
     {
@@ -246,7 +246,7 @@ module.exports = async () => {
         "https://www.petland.ca/cdn/shop/files/kong-kong-classic-dog-toy-29509678170214.png?v=1691378832",
       price: 1650,
       stock: 25,
-      subcategoryId: 19,
+      category: 19,
       featured: true,
     },
     {
@@ -256,7 +256,7 @@ module.exports = async () => {
         "https://www.woowguau.mx/cdn/shop/products/4680_Imagen1__52967_zoom_512x512.png?v=1602537723",
       price: 690,
       stock: 25,
-      subcategoryId: 20,
+      category: 20,
       featured: true,
     },
     {
@@ -266,7 +266,7 @@ module.exports = async () => {
         "https://png.pngtree.com/png-vector/20240807/ourmid/pngtree-more-less-or-equal-with-cute-pet-toys-png-image_13149917.png",
       price: 350,
       stock: 25,
-      subcategoryId: 21,
+      category: 21,
       featured: true,
     },
     {
@@ -276,7 +276,7 @@ module.exports = async () => {
         "https://w7.pngwing.com/pngs/353/219/png-transparent-child-toy-play-ball-pits-tent-tunnel-game-people-textile.png",
       price: 1950,
       stock: 25,
-      subcategoryId: 22,
+      category: 22,
       featured: true,
     },
     {
@@ -286,7 +286,7 @@ module.exports = async () => {
         "https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/desktop_can-cat-paleta-mordillo-ice_935",
       price: 720,
       stock: 25,
-      subcategoryId: 19,
+      category: 19,
       featured: true,
     },
     {
@@ -296,7 +296,7 @@ module.exports = async () => {
         "https://png.pngtree.com/png-clipart/20220927/original/pngtree-3dc4d-three-dimensional-plush-fruits-and-vegetables-carrots-png-image_8637568.png",
       price: 540,
       stock: 25,
-      subcategoryId: 21,
+      category: 21,
       featured: true,
     },
     {
@@ -306,7 +306,7 @@ module.exports = async () => {
         "https://c0.klipartz.com/pngpicture/11/77/gratis-png-anillo-de-caucho-natural-material-de-arandela-de-goma-anillo-thumbnail.png",
       price: 630,
       stock: 25,
-      subcategoryId: 19,
+      category: 19,
       featured: true,
     },
     {
@@ -316,7 +316,7 @@ module.exports = async () => {
         "https://png.pngtree.com/png-vector/20240317/ourmid/pngtree-colorful-toy-balls-png-image_11989170.png",
       price: 870,
       stock: 25,
-      subcategoryId: 20,
+      category: 20,
       featured: true,
     },
     {
@@ -326,7 +326,7 @@ module.exports = async () => {
         "https://inventario-petuniverse.s3.amazonaws.com/product_pics/numbell_dispensador_bolsa_celeste_sin_fondo.png",
       price: 480,
       stock: 25,
-      subcategoryId: 18,
+      category: 18,
       featured: true,
     },
     {
@@ -336,7 +336,7 @@ module.exports = async () => {
         "https://png.pngtree.com/png-clipart/20240227/original/pngtree-wind-up-vintage-toy-rabbit-material-photo-png-image_14427971.png",
       price: 430,
       stock: 25,
-      subcategoryId: 21,
+      category: 21,
       featured: true,
     },
     {
@@ -346,7 +346,7 @@ module.exports = async () => {
         "https://static.vecteezy.com/system/resources/previews/015/310/780/non_2x/donut-stacking-toy-with-transparent-background-png.png",
       price: 500,
       stock: 25,
-      subcategoryId: 19,
+      category: 19,
       featured: true,
     },
     {
@@ -356,7 +356,7 @@ module.exports = async () => {
         "https://png.pngtree.com/png-clipart/20231213/original/pngtree-fish-toy-character-of-finding-nemo-black-photo-png-image_13833691.png",
       price: 580,
       stock: 25,
-      subcategoryId: 22,
+      category: 22,
       featured: true,
     },
     {
@@ -366,7 +366,7 @@ module.exports = async () => {
         "https://w7.pngwing.com/pngs/1000/854/png-transparent-octopus-plush-stuffed-animals-cuddly-toys-teddy-bear-toy-game-marine-mammal-child.png",
       price: 670,
       stock: 25,
-      subcategoryId: 21,
+      category: 21,
       featured: true,
     },
     {
@@ -375,7 +375,7 @@ module.exports = async () => {
       image: "https://fancypets.mx/IMAGENES/FANCY%20PETS/1000x1000_72_png/FL9422/FL9422_E.png",
       price: 880,
       stock: 25,
-      subcategoryId: 22,
+      category: 22,
       featured: true,
     },
     {
@@ -385,7 +385,7 @@ module.exports = async () => {
         "https://www.somoswapp.com.ar/media/catalog/product/cache/4e187a3216125186f353d1ef896e6af4/p/e/pelota_huella_led_combinada.png",
       price: 2100,
       stock: 25,
-      subcategoryId: 20,
+      category: 20,
       featured: true,
     },
     {
@@ -394,8 +394,8 @@ module.exports = async () => {
       image: "",
       price: 1450,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Pedigree Cachorro Carne y Leche",
@@ -403,8 +403,8 @@ module.exports = async () => {
       image: "",
       price: 1200,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Eukanuba Control de Peso",
@@ -412,8 +412,8 @@ module.exports = async () => {
       image: "",
       price: 2100,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Royal Canin Mini Adult",
@@ -421,8 +421,8 @@ module.exports = async () => {
       image: "",
       price: 1800,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Balance Natural Cordero y Arroz",
@@ -430,8 +430,8 @@ module.exports = async () => {
       image: "",
       price: 2300,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Old Prince Grain Free",
@@ -439,8 +439,8 @@ module.exports = async () => {
       image: "",
       price: 2400,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "ProPlan Sensitive Skin & Stomach",
@@ -448,8 +448,8 @@ module.exports = async () => {
       image: "",
       price: 2600,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Canbo Adulto Carne",
@@ -457,8 +457,8 @@ module.exports = async () => {
       image: "",
       price: 1100,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Dogui Croquetas",
@@ -466,8 +466,8 @@ module.exports = async () => {
       image: "",
       price: 850,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Raza Premium Cordero",
@@ -475,8 +475,8 @@ module.exports = async () => {
       image: "",
       price: 1650,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Royal Canin Maxi Junior",
@@ -484,8 +484,8 @@ module.exports = async () => {
       image: "",
       price: 2550,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Kongo Cachorro Pollo y Arroz",
@@ -493,8 +493,8 @@ module.exports = async () => {
       image: "",
       price: 1300,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "VitalCan Balanced Adulto",
@@ -502,8 +502,8 @@ module.exports = async () => {
       image: "",
       price: 1600,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "ProPlan Puppy Large Breed",
@@ -511,8 +511,8 @@ module.exports = async () => {
       image: "",
       price: 2750,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Excellent Pollo y Arroz",
@@ -520,8 +520,8 @@ module.exports = async () => {
       image: "",
       price: 2000,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Natura Diet Grain Free",
@@ -529,8 +529,8 @@ module.exports = async () => {
       image: "",
       price: 2800,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Sabrositos Adulto Carne",
@@ -538,8 +538,8 @@ module.exports = async () => {
       image: "",
       price: 790,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Agility Gold Skin Care",
@@ -547,8 +547,8 @@ module.exports = async () => {
       image: "",
       price: 2050,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "Nutrique Senior Dog",
@@ -556,8 +556,8 @@ module.exports = async () => {
       image: "",
       price: 2900,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
     {
       name: "DogSelection Premium",
@@ -565,8 +565,8 @@ module.exports = async () => {
       image: "",
       price: 1350,
       stock: 25,
-      subcategoryId: 1,
-      featured: false,
+      category: 1,
+      featured: true,
     },
   ];
   await Product.bulkCreate(products);
